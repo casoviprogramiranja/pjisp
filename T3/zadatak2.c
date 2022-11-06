55de07945dca636831339d0614f78615836bfd68
Casovi Programiranja <113212417+casoviprogramiranja@users.noreply.github.com>
2022-11-06 12:30:23 +0100

    Update zadatak2.c

--- T3/zadatak2.c
+++ T3/zadatak2.c
@@ -14,7 +14,7 @@ int main() {
 	do {
 		printf("Unesi duzinu niza:");
 		scanf("%d", &n);
-	} while (n <= 0);
+	} while (n <= 0 || n > MAX);
 	
 	for(i = 0; i < n; i++) {
 		printf("Unesi a[%d] =", i);
